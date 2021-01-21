5347b9d431fb6a62553932065b55a730ed9fc0fa
boyboy0710 <boyboy0710@naver.com>
2021-01-21 13:01:18 +0900

    Add files via upload

--- mob/src/com/boyboy0710/mob/Main.java
+++ mob/src/com/boyboy0710/mob/Main.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Skeleton.SkeletonType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
